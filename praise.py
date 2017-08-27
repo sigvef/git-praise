99d1517bf2c13a0019525b75352425e05191d0f2
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-08-27 21:42:46 +0200

    Refactor praise.py a bit

--- praise.py
+++ praise.py
@@ -8,41 +8,19 @@ import math
 import os
 import pygments
 
+formatter = get_formatter_by_name('console')
 
-def praise(filename, repo):
-    formatter = get_formatter_by_name('console')
-    terminal_height, terminal_width = os.popen(
-        'stty size', 'r').read().split()
-    with open(filename) as f:
-        text = f.read()
+def highlight(filename, text):
     lexer = get_lexer_for_filename(filename)
-    highlighted = pygments.highlight(
-        text, lexer, formatter).split('\n')
-    blame_entries = repo.blame_incremental('HEAD', filename)
-    sorted_blame_entries = sorted(
-        blame_entries, key=lambda entry: entry.orig_linenos[0])
-    display_entries = []
-    for entry in sorted_blame_entries:
-        line_numbers = entry.orig_linenos
-        start = entry.orig_linenos[0] - 1
-        end = entry.orig_linenos[-1]
-        lines = highlighted[start:end]
-        display_entries.append(
-            DisplayEntry(commit=entry.commit,
-            lines=lines,
-            line_number_start=start + 1))
-    number_of_display_entries = len(display_entries)
-    name_length = max([len(display_entry.name)
-                       for display_entry in display_entries])
-    author_name_length = max([
-        len(display_entry.author_name)
-        for display_entry in display_entries])
+    return pygments.highlight(text, lexer, formatter).split('\n')
+
+def header(filename, repo, sidebar_width):
     relative_path = repo.tree()[filename].path
     basename = os.path.basename(relative_path)
     formatted_relative_path = leftpad(
         '(repo)/' + relative_path[:-len(basename)],
-        20)
-    header = ''.join([
+        sidebar_width)
+    return ''.join([
         colored(formatted_relative_path,
                 'grey',
                 attrs=['dark', 'underline']),
@@ -54,11 +32,48 @@ def praise(filename, repo):
                 attrs=['bold', 'underline']),
     ])
 
-    print(header)
+def praise(filename, repo):
+    with open(filename) as f:
+        text = f.read()
+    highlighted = highlight(filename, text)
+    blame_entries = sorted(
+        repo.blame_incremental('HEAD', filename),
+        key=lambda entry: entry.orig_linenos[0])
+
+    # make display entires
+    display_entries = []
+    for entry in blame_entries:
+        line_numbers = entry.orig_linenos[0]
+        start = entry.orig_linenos[0] - 1
+        end = entry.orig_linenos[-1]
+        lines = highlighted[start:end]
+        display_entries.append(
+            DisplayEntry(commit=entry.commit,
+            lines=lines,
+            line_number_start=start + 1))
+
+    # measure display entires
+    name_length = max([len(display_entry.name)
+                       for display_entry in display_entries])
+    author_name_length = max([
+        len(display_entry.author_name)
+        for display_entry in display_entries])
+
+    line_number_width = int(math.log(len(highlighted), 10) + 0.5)
+
+    sidebar_width = sum((
+        name_length,
+        author_name_length,
+        19,
+        line_number_width))
+
+    print(header(filename, repo, sidebar_width))
+    terminal_height, terminal_width = os.popen(
+        'stty size', 'r').read().split()
     for display_entry in display_entries:
         line = display_entry.render(
             author_name_length=author_name_length,
             name_length=name_length,
-            total_line_count=number_of_display_entries,
+            total_line_count=len(highlighted),
             width=int(terminal_width))
         print(line)
